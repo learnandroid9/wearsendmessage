e99d1dfdd36e74cbe6df94b03c18d1c8ac58a939
kaye wu <apiswswsw@gmail.com>
2014-11-29 22:31:49 -0800

    send message from mobile to wear device.  no receiving code yet on wear side.

--- mobile/src/main/java/me/xbt/wearsendmessage/PhoneActivity.java
+++ mobile/src/main/java/me/xbt/wearsendmessage/PhoneActivity.java
@@ -1,28 +1,129 @@
 package me.xbt.wearsendmessage;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.wearable.MessageApi;
+import com.google.android.gms.wearable.Node;
+import com.google.android.gms.wearable.NodeApi;
+import com.google.android.gms.wearable.Wearable;
 
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class PhoneActivity extends Activity {
 
+    private static final String TAG = "PhoneActivity: ";
+    GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phone);
 
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(new GoogleApiClient.ConnectionCallbacks() {
+                    @Override
+                    public void onConnected(Bundle connectionHint) {
+                        Log.d(TAG, "onConnected: " + connectionHint);
+                        //  "onConnected: null" is normal.
+                        //  There's nothing in our bundle.
 
+                        // Now you can use the Data Layer API
+//                        List<Node> nodes = getNodes();
+//                        if (nodes.size() > 0) {
+//                            Log.d(TAG, "number of nodes=" + nodes.size());
+//                            Log.i(TAG, "nodes=" + nodes);
+//
+//                            Node node = nodes.get(0); // there should be only one node
+//                            MessageApi.SendMessageResult result = Wearable.MessageApi.sendMessage(
+//                                    mGoogleApiClient, node.getId(), START_ACTIVITY_PATH, null).await();
+//                            if (!result.getStatus().isSuccess()) {
+//                                Log.e(TAG, "ERROR: failed to send Message: " + result.getStatus());
+//                            }
+//                        }
+                        tellWatchConnectedState("connected");
+                    }
+                    @Override
+                    public void onConnectionSuspended(int cause) {
+                        Log.d(TAG, "onConnectionSuspended: " + cause);
+                    }
+                })
+                .addOnConnectionFailedListener(new GoogleApiClient.OnConnectionFailedListener() {
+                    @Override
+                    public void onConnectionFailed(ConnectionResult result) {
+                        Log.d(TAG, "onConnectionFailed: " + result);
+                    }
+                })
+                        // Request access only to the Wearable API
+                .addApi(Wearable.API)
+                .build();
+
+        // need to add <meta-data> com.google.android.gms.version tag into androidmanifest.xml
+        mGoogleApiClient.connect();
 
     }
 
 
 
+    private void tellWatchConnectedState(final String state){
+
+        // it is necessary to put the following code in asynctask
+        // getNodes() uses await(), which cannot be used on ui thread.
+        // we will see an error about await cannot be called on ui thread if we do not use asynctask.
+        new AsyncTask<Void, Void, List<Node>>(){
+
+            @Override
+            protected List<Node> doInBackground(Void... params) {
+                return getNodes();
+            }
+
+            @Override
+            protected void onPostExecute(List<Node> nodeList) {
+                for(Node node : nodeList) {
+                    Log.v(TAG, "telling " + node.getId() + " i am " + state);
+
+                    PendingResult<MessageApi.SendMessageResult> result = Wearable.MessageApi.sendMessage(
+                            mGoogleApiClient,
+                            node.getId(),
+                            "/listener/lights/" + state,
+                            null
+                    );
+
+                    result.setResultCallback(new ResultCallback<MessageApi.SendMessageResult>() {
+                        @Override
+                        public void onResult(MessageApi.SendMessageResult sendMessageResult) {
+                            Log.v(TAG, "Phone: " + sendMessageResult.getStatus().getStatusMessage());
+                        }
+                    });
+                }
+            }
+        }.execute();
+
+    }
+
+
+    /**
+     * get a list of connected nodes that you can potentially send messages to
+     */
+    private List<Node> getNodes() {
+        ArrayList<Node> results = new ArrayList<Node>();
+        NodeApi.GetConnectedNodesResult nodes =
+                Wearable.NodeApi.getConnectedNodes(mGoogleApiClient).await();
+        for (Node node : nodes.getNodes()) {
+            results.add(node);
+        }
+        return results;
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
