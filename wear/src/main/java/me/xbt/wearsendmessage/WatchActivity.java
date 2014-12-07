697a271083780513f7633ecda646dd42252f5fe2
kaye wu <apiswswsw@gmail.com>
2014-12-06 17:02:51 -0800

    wear side, when msg is received, display msg content and # of msg received.

--- wear/src/main/java/me/xbt/wearsendmessage/WatchActivity.java
+++ wear/src/main/java/me/xbt/wearsendmessage/WatchActivity.java
@@ -7,6 +7,14 @@ import android.widget.TextView;
 
 public class WatchActivity extends Activity {
 
+    /**
+     * parameter name for msg.
+     * used in intent.putExtra() to pass in a parameter.
+     */
+    public static final String PARAM_MSG = "param_msg";
+    /** # of msg received */
+    public static final String PARAM_NUM_MSG = "param_num_msg";
+
     private TextView mTextView;
 
     @Override
@@ -18,7 +26,26 @@ public class WatchActivity extends Activity {
             @Override
             public void onLayoutInflated(WatchViewStub stub) {
                 mTextView = (TextView) stub.findViewById(R.id.text);
+                TextView textView2 = (TextView) stub.findViewById(R.id.text2);
+
+                // get params passed to this intent.
+                Bundle extras = getIntent().getExtras();
+                if (extras != null) {
+                    String msg = extras.getString(PARAM_MSG);
+                    int numMsg = extras.getInt(PARAM_NUM_MSG);
+                    if (msg != null) {
+                        mTextView.setText("msg received=" + msg);
+                    }
+                    textView2.setText("# of msgs received: " + numMsg);
+                }
             }
         });
+    }
+
+    /** display the text to user */
+    public void setText(String msg) {
+        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
+        TextView textView = (TextView) stub.findViewById(R.id.text);
+        textView.setText(msg);
     }
 }
